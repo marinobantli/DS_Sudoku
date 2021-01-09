3028fd357198d9785e6c17406e60e6bed496f086
marinobantli <zewmim-4jEqgo-feqbax>
2021-01-09 17:09:59 +0100

    some code cleaning and preparation for JSON generation

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import imageProcessor
+import jsonCreator
 import ocrEngine
 import numpy
+import tkinter
+from tkinter import filedialog
 
 #Vars
-imgPath         = 'D:\\Private\\Juventus\\DataScience\\resources\\sudoku6.jpg'
-img             = imageProcessor.processImage(imgPath)
-sudokuSize      = 9
+sudokuSize  = 9
+sudokuData  = numpy.empty((sudokuSize, sudokuSize), dtype=object)
 
-#X, Y
-sudokuData      = numpy.empty((sudokuSize, sudokuSize), dtype=object)
+#Prerequisites
+root        = tkinter.Tk().withdraw() #Don't show tkinter root window
+imgPath     = filedialog.askopenfilename()
+img         = imageProcessor.processImage(imgPath)
 
+print("Sudoku puzzle path:",imgPath)
+print("Processing puzzle...")
+
+#Process the Puzzle
 for x in range(0, sudokuSize):
     for y in range(0, sudokuSize):
-        image = imageProcessor.extractDigit(img,x,y)
-        result = ''
+        result  = ''
+        image   = imageProcessor.extractDigit(img, x, y)
 
         if image is not None:
             result = ocrEngine.getChar(image)
-            #print(result.strip())
+            # print(result.strip())
 
         if result.strip() == '':
             result = 0
+
         sudokuData[x, y] = int(result)
 
+print("Processing done.")
+print("Results:")
+print("---------------------")
 print(sudokuData)
-
-#print(ocrEngine.getChar(imageProcessor.getCell(imageProcessor.processImage(imgPath), 3, 0)))
+print("---------------------")
+print("Generating JSON file")
+jsonCreator.createJSON(sudokuData, sudokuSize)
+print("JSON generated.")
